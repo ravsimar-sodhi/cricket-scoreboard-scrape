5ae9c215e44e749b6707713474974c28d14e85dd
Ravsimar Sodhi <ravsimar.sodhi@research.iiit.ac.in>
2019-02-22 21:32:28 +0530

    fixed bug in generation, added synthetic data for testing

--- src/synth_data_generate.py
+++ src/synth_data_generate.py
@@ -2,26 +2,34 @@
 
 import random
 OUTPUT="synth_data.txt"
-TABLECOUNT= 100
+TABLECOUNT= 10000
 ROWCOUNT = 2
 
 PLAYERS = ["PP Shaw", "KL Rahul", "CA Pujara"]
-DATA_FIELDS = ["R", "B", "M", "SR"]
+DATA_FIELDS = ["Player", "R", "B", "M", "SR"]
 
 PATTERN = '''1 {}: {}, {} : {}, {} : {} .
 2 {} : {}, {} : {}, {} : {} .
-3 What is the {} in {}?\t{}\t{}
+3 What is the {} of {}?\t{}\t{}
+'''
+
+'''1 Player : {}, R : {}, B : {} .
+2 Player : {}, R : {}, B : {} .
+3 What is the R of {}?\t{}\t1
+4 What is the B of {}?\t{}\t2
+5 What is the B of {}?\t{}\t2
 '''
 
 def synth_data_generate():
-    with open(OUTPUT, 'w') as f:
+    f = DATA_FIELDS
+    with open(OUTPUT, 'w') as file:
         for i in range(TABLECOUNT):
             v = []
             players = PLAYERS[:]
             v0 = []
             randPlayer1 = random.choice(players)
             v0.append(randPlayer1)
-            players.remove(randPlayer)
+            players.remove(randPlayer1)
             randPlayer2 = random.choice(players)
             v0.append(randPlayer2)
             v.append(v0)
